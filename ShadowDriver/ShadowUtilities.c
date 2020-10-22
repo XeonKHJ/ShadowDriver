2898e667be8e11b16098ea6e52db5d98e141e9fe
康宏嘉 <redalertkhj@live.cn>
2020-10-23 00:04:58 +0800

    在实用工具中添加计算校验和函数。

--- ShadowDriver/ShadowUtilities.c
+++ ShadowDriver/ShadowUtilities.c
@@ -50,3 +50,28 @@ void ConvertBytesArrayToHexString(char* bytes, size_t bytesLength, char* outputS
 
 	*currentOutputPos = 0;
 }
+
+/// <summary>
+/// 计算TCP/IP校验和
+/// </summary>
+/// <param name="bytes">要计算校验和的缓冲区指针，记得把校验和所在的位置置零再传进来。</param>
+/// <param name="byteCounts">缓冲区大小（字节）</param>
+/// <returns>校验和</returns>
+unsigned short CalculateCheckSum(char* bytes, int byteCounts)
+{
+	unsigned int sum = 0;
+	for (int i = 0; i < byteCounts; i += 2)
+	{
+		unsigned int perSum = (unsigned int)(bytes[i + 1] & 0xff) + (((unsigned int)((bytes[i])) << 8) & 0xff00);
+		sum += perSum;
+	}
+
+	while (sum > 0xffff)
+	{
+		unsigned int exceedPart = (sum & (~0xFFFF)) >> 16;
+		unsigned int remainPart = sum & 0xffff;
+		sum = remainPart + exceedPart;
+	}
+
+	return ~(sum & 0xFFFF);
+}
