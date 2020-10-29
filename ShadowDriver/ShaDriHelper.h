b8fd853971d385ba4934af1f61e5f756f2368e4b
康宏嘉 <redalertkhj@live.cn>
2020-10-30 01:36:39 +0800

    把一些和数据包有关的计算扔到ShaDriHelper里。

--- ShadowDriver/ShaDriHelper.h
+++ ShadowDriver/ShaDriHelper.h
@@ -1,12 +1,9 @@
 #pragma once
-typedef struct ShaDrTcpRawHeader
-{
-	SHORT SourcePort;
-	SHORT DestPort;
-	UINT32 SequenceNumber;
-	UINT32 AcknowLedgmentNumber;
-	SHORT HeaderLengthAndFlags;
-	SHORT WindowSize;
-	SHORT CheckSum;
-	SHORT UrgentPoint;
-}ShaDrTcpRawHeader;
+#include "ShadowUtilities.h"
+#include "driver.h"
+#include "fwpsk.h"
+
+void ConvertNetBufferListToTcpRawPacket(_Out_ ShadowTcpRawPacket** tcpRawPacketPoint, _In_ PNET_BUFFER_LIST nbl, _In_ UINT32 nblOffset);
+void CalculateTcpPacketCheckSum(short transportDataLength, PCHAR mdlCharBuffer, CHAR protocal, short ipHeaderLength, PNET_BUFFER_LIST packet);
+void DeleteTcpRawPacket(_In_ ShadowTcpRawPacket* tcpRawPacket);
+void PrintNetBufferList(PNET_BUFFER_LIST packet, ULONG level);
