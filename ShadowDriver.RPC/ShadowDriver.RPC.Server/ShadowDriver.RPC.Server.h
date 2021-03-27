f255a0d4d4b5ed75edb1fed07ee4b68212203abc
康宏嘉 <redalertkhj@live.cn>
2021-03-28 00:27:01 +0800

    把没用的项目删了

--- ShadowDriver.RPC/ShadowDriver.RPC.Server/ShadowDriver.RPC.Server.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-
-long InitializeShadowDriverRPCServer();
--- ShadowDriver.RPC/ShadowDriver.RPC.Server/main.c
+++
@@ -1,9 +0,0 @@
-#include "../ShadowDriver.RPC.Common/ShadowDriverRPC.h"
-#include "ShadowDriver.RPC.Server.h"
-
-int main()
-{
-	RPC_STATUS status = InitializeShadowDriverRPCServer();
-
-	return status;
-}
