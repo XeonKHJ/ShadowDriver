4cc2965c98affa6f565f5f086b2b37f45e58322d
康宏嘉 <redalertkhj@live.cn>
2021-04-27 13:52:49 +0800

    Delete redundant files.

--- ShadowDriver/ShaDriHelper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ShadowUtilities.h"
-#include "fwpsk.h"
-
-void ConvertNetBufferListToTcpRawPacket(_Out_ ShadowTcpRawPacket** tcpRawPacketPoint, _In_ PNET_BUFFER_LIST nbl, _In_ UINT32 nblOffset);
-void CalculateTcpPacketCheckSum(short transportDataLength, PCHAR mdlCharBuffer, CHAR protocal, short ipHeaderLength, PNET_BUFFER_LIST packet);
-void DeleteTcpRawPacket(_In_ ShadowTcpRawPacket* tcpRawPacket);
-void PrintNetBufferList(PNET_BUFFER_LIST packet, ULONG level);
