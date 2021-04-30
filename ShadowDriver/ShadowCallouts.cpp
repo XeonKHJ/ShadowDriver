36605871701d1dbb4b289eb209c6b009d5ee5c41
康宏嘉 <redalertkhj@live.cn>
2021-04-30 22:09:58 +0800

    Fix some issues.

--- ShadowDriver/ShadowCallouts.cpp
+++ ShadowDriver/ShadowCallouts.cpp
@@ -73,7 +73,46 @@ VOID NTAPI NetworkInV4ClassifyFn(
 	_Inout_ FWPS_CLASSIFY_OUT0* classifyOut
 )
 {
+	NTSTATUS status = STATUS_SUCCESS;
+	NET_BUFFER_LIST* packet;
+	FWPS_STREAM_DATA* streamData;
+	SIZE_T dataLength = 0;
+	SIZE_T bytes = 0;
+	NDIS_STATUS ndisStatus;
+
+	PNET_BUFFER_LIST clonedPacket = NULL;
+	FWPS_PACKET_INJECTION_STATE injectionState = FWPS_PACKET_NOT_INJECTED;
+
+	packet = (NET_BUFFER_LIST*)layerData;
+#ifdef DBG
+	DbgPrintEx(DPFLTR_IHVNETWORK_ID, DPFLTR_TRACE_LEVEL, "Original Net Buffer List:\t\n");
+	//PrintNetBufferList(packet, DPFLTR_TRACE_LEVEL);
+#endif
 	classifyOut->actionType = FWP_ACTION_PERMIT;
+
+	if (packet)
+	{
+		ShadowFilterContext* context = (ShadowFilterContext*)(filter->context);
+
+		if (context->NetPacketFilteringCallout != NULL)
+		{
+			//+++++++++++将数据包包装成自己的形式以便进行处理+++++++++++
+			status = FwpsAllocateCloneNetBufferList0(packet, NULL, NULL, 0, &clonedPacket);
+			PNET_BUFFER netBuffer = NET_BUFFER_LIST_FIRST_NB(clonedPacket);
+			PBYTE dataBuffer = (PBYTE)NdisGetDataBuffer(netBuffer, NET_BUFFER_DATA_LENGTH(netBuffer), NULL, 1, 0);
+#ifdef DBG
+			DbgPrintEx(DPFLTR_IHVNETWORK_ID, DPFLTR_INFO_LEVEL, "Packet received!\t\n");
+#endif
+			(context->NetPacketFilteringCallout)(NetLayer::NetworkLayer, NetPacketDirection::Out, dataBuffer, NET_BUFFER_DATA_LENGTH(netBuffer), context->CustomContext);
+		}
+		if (context->IsModificationEnable)
+		{
+			//还未实现
+		}
+
+		//删除分配的缓冲区
+
+	}
 }
 
 
